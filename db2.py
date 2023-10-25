cfecffee734debbae118e97de1344b3250e07ba3
Youngnam0304 <lifehasme@naver.com>
2023-10-25 16:51:40 +0900

    db 연습 23

--- db2.py
+++ db2.py
@@ -1,8 +1,8 @@
 #db1.py
 import sqlite3
 
-#연결객체 :보통 정해져있음. 아래와 같이 
-con = sqlite3.connect(":memory:")
+#연결객체(파일에 저장)
+con = sqlite3.connect("c:\\work\\sample.db") 
 #커서객체 
 cur = con.cursor()
 #테이블 구조 생성
@@ -28,13 +28,11 @@ cur.executemany("insert into PhoneBook (name, phoneNum) values (?, ?);",
 
 #검색
 cur.execute("select * from PhoneBook;")
-print("---fetchone()---")  #fetchone : 레코드 포인터가 하나 인경우 하나의 데이터를 읽고 버퍼(임시메모리)에다가 보여주고 사라짐 그리고 다음으로 넘어감 
-print(cur.fetchone())
-print("---fetchmany(2)---") #fetchmany : 두개의 데이터(다수)의 데이터를 읽고 보여주고 사라짐. 그리고 다음으로 넘어감
-print(cur.fetchmany(2))
-print("---fetchall()---") #fetchall : 모든 데이터를 보여줌. 그러나 이번 예제에는 앞에 one / many에서 1/ 2를 보여주고 사라짐으로 안보임
-cur.execute("select * from PhoneBook;") # 이전 one / many에서 사라진 버퍼의 데이터를 select 구문을 사용하여 버퍼 메모리를 다시 불러옴 (reset) 라는 말.  
 print(cur.fetchall())
+
+#작업 완료(무조건 작업 완료 전에 commit하고 마무리 필요. 안그러면 rollback함)
+con.commit()
+
 
 # #검색
 # cur.execute("select * from PhoneBook;")
